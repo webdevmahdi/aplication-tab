a816351c611d48e68bdc0a0d518812d8ddd12dfb
webdevmahdi <mahdihasan01705259@gmail.com>
2023-07-25 00:41:37 +0600

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+let putElements = () => {
+    let storageID = document.getElementById('storage-keys');
+    let idContent = storageID.value;
+    let storageValues = document.getElementById('storage-values');
+    let valuesContent = storageValues.value;
+
+    function makeString(keys, values) {
+        let valueSTR = JSON.stringify(values);
+        localStorage.setItem(keys, valueSTR)
+    }
+
+    let obj = { name: 'mahdi', hobby: 'programming' };
+    makeString('object', obj);
+    let array = [12, 45, 78, 32];
+    makeString('array', array)
+
+    if (idContent && valuesContent) {
+        localStorage.setItem(idContent, valuesContent)
+    }
+    storageID.value = '';
+    storageValues.value = '';
+}
